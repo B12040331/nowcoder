d6f1a72a6d7d8b157de086f4e69d9ce59b49b52f
B12040331 <2464847121@qq.com>
2015-05-03 14:07:03 +0800

    调整数组顺序使奇数位于偶数前面.cpp

--- 调整数组顺序使奇数位于偶数前面.cpp
+++ 调整数组顺序使奇数位于偶数前面.cpp
@@ -1,14 +1,39 @@
 
 #include <iostream>
+#include <vector>
 using namespace std;
 
 class Solution {
 public:
 	void reOrderArray(vector<int> &array) {
-
+	    vector<int> even, odd;
+	    for(vector<int>::iterator it=array.begin(); it!=array.end(); ++it) {
+            if(*it & 1) {
+                odd.push_back(*it);
+            }
+            else {
+                even.push_back(*it);
+            }
+	    }
+	    array.clear();
+	    for(vector<int>::iterator it=odd.begin(); it!=odd.end(); ++it) {
+            array.push_back(*it);
+	    }
+	    for(vector<int>::iterator it=even.begin(); it!=even.end(); ++it) {
+            array.push_back(*it);
+	    }
 	}
 };
 
 int main() {
+    Solution solution;
+    vector<int> vec;
+    for(int i=0; i<10; ++i) {
+        vec.push_back(i);
+    }
+    solution.reOrderArray(vec);
+    for(int i=0; i<10; ++i) {
+        cout << vec[i] << " ";
+    }
     return 0;
 }
